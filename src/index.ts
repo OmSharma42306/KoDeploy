921cac3ec91fc6c18688f7adef29f5c35f29330a
Om Sharma <omsharma.83173@gmail.com>
2025-05-02 21:58:31 +0530

    feat:initialized-the-endpoint-for-user-to-send-repo-url

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,35 @@
 import express from "express";
-
+import {Request,Response} from "express"
 
 const app = express();
 const PORT = 3000;
+
+
+// Initializing an Endpoint for user to send repo url
+
+
+app.post('/send-repo-url',async (req:Request,res:Response)=>{
+    
+    try{
+        const repoUrl = req.body.repoUrl;
+        
+        
+        
+        
+        
+        res.json({msg:"Done"})
+
+    }catch(error){
+        res.json({error});
+    }
+    
+    
+    return;
+})
+
+
+
+
 
 
 app.listen(PORT,()=>{

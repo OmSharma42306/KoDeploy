09f39fb8fbeef496b87aa85b16d6b8f583c88998
Om Sharma <omsharma.83173@gmail.com>
2025-05-02 22:10:11 +0530

    service:initiated-upload-service

--- upload-service/src/index.ts
+++ upload-service/src/index.ts
@@ -1,21 +1,30 @@
 import express from "express";
 import {Request,Response} from "express"
-
+import cors from "cors";
+import simpleGit from "simple-git";
 const app = express();
 const PORT = 3000;
 
-
+// Neccessary Middlewares.
+app.use(cors())
+app.use(express.json());
 // Initializing an Endpoint for user to send repo url
+
+function generate(){
+    const subset = "123456789qwertyuiopasdfghjklzxcvbnm";
+    const length = 5;
+    let id = "";
+    for(let i = 0;i<length;i++){
+        id += subset[Math.floor(Math.random()*subset.length)]
+    }
+    return id;  
+}
 
 
 app.post('/send-repo-url',async (req:Request,res:Response)=>{
     
     try{
         const repoUrl = req.body.repoUrl;
-        
-        
-        
-        
         
         res.json({msg:"Done"})
 
